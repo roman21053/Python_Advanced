bf9646d3cd834a36a7a4493fd3c4d6bbc0986323
roman21053 <roman21053@gmail.com>
2023-05-21 17:22:09 +0300

    added files .gitignor in Django

--- Home_Work/Django/HW_Less_20_Django_intro/cursor/main/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import MenuItem, SliderItem
-
-admin.site.register(MenuItem)
-admin.site.register(SliderItem)
--- Home_Work/Django/HW_Less_20_Django_intro/cursor/main/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .models import MenuItem
-
-
-def main(request):
-    menu_items = MenuItem.objects.all()
-    return render(request, "index.html", {"menu_items": menu_items})
