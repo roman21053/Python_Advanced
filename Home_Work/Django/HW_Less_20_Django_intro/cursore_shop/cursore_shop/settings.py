85a4bf74d2a930b85e0648f4d73e4401ba932a8a
roman21053 <roman21053@gmail.com>
2023-05-27 18:02:11 +0300

    finish

--- Home_Work/Django/HW_Less_20_Django_intro/cursore_shop/cursore_shop/settings.py
+++ Home_Work/Django/HW_Less_20_Django_intro/cursore_shop/cursore_shop/settings.py
@@ -121,8 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_ROOT = BASE_DIR, 'media'
+STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 # Default primary key field type
