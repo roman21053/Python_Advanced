37b5ff245e41801c33f4e6ba2e6e946bb9bb4cc1
roman21053 <roman21053@gmail.com>
2023-05-29 15:55:41 +0300

    start HW_Less_22

---
+++ Home_Work/Django/HW_Less_22_Django_ORM/cursore_shop/main/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import MenuItem, SliderItem
+
+# Create your views here.
+def main(request):
+    menu_items = MenuItem.objects.all()
+    slider_items = SliderItem.objects.all()
+    return render(request,'index.html', {"menu_items": menu_items, "slider_items": slider_items})
