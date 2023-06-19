0c19af0aa3dc850542a1342dfe164b084c59d6cf
roman21053 <roman21053@gmail.com>
2023-06-19 11:34:57 +0300

    finished HW_Less_22

--- Home_Work/Django/HW_Less_22_Django_ORM/cursore_shop/cursore_shop/urls.py
+++ Home_Work/Django/HW_Less_22_Django_ORM/cursore_shop/cursore_shop/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-    path("products", include("products.urls")),
+    path('products', include('products.urls')),
 ]
 
 if settings.DEBUG:
--- Home_Work/Django/HW_Less_22_Django_ORM/cursore_shop/products/urls.py
+++ Home_Work/Django/HW_Less_22_Django_ORM/cursore_shop/products/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import category_page
+from .views import category_page, product_page
 
 urlpatterns = [
-    path("/category/<slug>", category_page, name="category_page")
+    path("/<slug>", category_page, name="category_page"),
+    path("/<slug>/<title>", product_page, name="product_page"),
 ]
